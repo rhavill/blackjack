463c5d06b8ecb5075bdf537d03baa818de169136
Ricardo Havill <rhavill2002@yahoo.com>
2015-10-25 00:01:58 -0400

    Display card back image when card is face down.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,23 @@ import {suitSymbol} from '../data/card';
 
 export default class Card extends React.Component {
 
+    isFaceUp() {
+        return this.props.card.isFaceUp;
+    }
+
     render() {
         let rank = this.props.card.rank;
         let suit = suitSymbol(this.props.card.suit);
         return (
             <div className="card">
-                <div className="front">
-                    <div className="index">{rank}<br/>{suit}</div>
-                    <div className="ace">{suit}</div>
-                    <div className="bottom-index">{rank}<br/>{suit}</div>
-                </div>
+                {this.isFaceUp() ?
+                    <div className="front">
+                        <div className="index">{rank}<br/>{suit}</div>
+                        <div className="ace">{suit}</div>
+                        <div className="bottom-index">{rank}<br/>{suit}</div>
+                    </div> :
+                    null
+                }
             </div>
         )
     }
