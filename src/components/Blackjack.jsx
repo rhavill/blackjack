8e690dca67f0260b061743835d3367fa1f54d998
Ricardo Havill <rhavill2002@yahoo.com>
2015-10-24 21:27:47 -0400

    Added Dealer component.

--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -1,6 +1,9 @@
 import React from 'react/addons';
+//import React from 'react/addons';
+//require('react-addons-{addon}')
 import {connect} from 'react-redux';
 import * as actionCreators from '../action_creators';
+import Dealer from './Dealer';
 
 export const Blackjack = React.createClass({
     mixins: [React.addons.PureRenderMixin],
@@ -11,6 +14,7 @@ export const Blackjack = React.createClass({
         return <div className="cards">
             <button onClick={() => this.props.shuffle()}>Shuffle</button>
             <button onClick={() => this.props.deal()}>Deal</button>
+            <Dealer/>
             {this.getCards().map(card =>
                     <h1>{card.rank} of {card.suit}</h1>
             )}
---
+++ src/components/Dealer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Dealer extends React.Component {
+    render() {
+        return (
+            <div id="dealer">
+                <h1>dealer</h1>
+            </div>
+        )
+    }
+}
