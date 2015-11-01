9a7008726e9035fda5a7b27d6162fcaa02122563
Ricardo Havill <rhavill2002@yahoo.com>
2015-11-01 01:02:17 -0400

    Changed deck to full deck.

--- src/index.jsx
+++ src/index.jsx
@@ -7,18 +7,7 @@ import reducer from './reducer';
 import actionMiddleware from './action_middleware';
 import {setState} from './action_creators';
 import {BlackjackContainer} from './components/Blackjack';
-//import cards from './data/cards';
-
-let cards = List.of(
-    Map({rank: '2', suit: 'C'}),
-    Map({rank: '10', suit: 'S'}),
-    Map({rank: '3', suit: 'S'}),
-    Map({rank: '4', suit: 'S'}),
-    Map({rank: '5', suit: 'S'}),
-    Map({rank: '6', suit: 'S'}),
-    Map({rank: '7', suit: 'S'}),
-    Map({rank: '8', suit: 'S'})
-);
+import cards from './data/cards';
 
 const createStoreWithMiddleware = applyMiddleware(actionMiddleware)(createStore);
 const store = createStoreWithMiddleware(reducer);
@@ -29,20 +18,6 @@ store.dispatch(setState({
     scores: [ [], [] ]
 }));
 
-//let autoDispatched = false;
-//let unsubscribe = store.subscribe(function() {
-//    let turn = store.getState().get('turn');
-//    let autoDispatched = store.getState('autoDispatched');
-//    console.log('store maybe changed! new turn?',turn), 'auto?', autoDispatched;
-//    if (turn == 'dealer' && !autoDispatched) {
-//        autoDispatched = true;
-//        store.setState(')
-//        store.dispatch({type:'DEALER_TURN'});
-//    }
-//    else {
-//        autoDispatched = false;
-//    }
-//});
 ReactDOM.render(
     <Provider store={store}>
         <BlackjackContainer />
