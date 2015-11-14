a825912484c774c69b39cadddae2a77049473dbf
Ricardo Havill <rhavill2002@yahoo.com>
2015-11-14 09:05:09 -0500

    Refactored test script by replacing re-used data structures with a variable.

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -3,84 +3,55 @@ import {expect} from 'chai';
 import reducer from '../src/reducer';
 import cards from '../src/data/cards';
 
+let jsState = {
+    deck: [
+        {rank: 'A', suit: 'C'},
+        {rank: '4', suit: 'S'},
+        {rank: 'T', suit: 'D'},
+        {rank: '2', suit: 'H'},
+        {rank: 'Q', suit: 'C'}
+    ]
+};
+
+let immutableState = Map({
+    deck: List.of(
+        Map({rank: 'A', suit: 'C'}),
+        Map({rank: '4', suit: 'S'}),
+        Map({rank: 'T', suit: 'D'}),
+        Map({rank: '2', suit: 'H'}),
+        Map({rank: 'Q', suit: 'C'})
+    )
+});
+
 describe('reducer', () => {
 
     it('handles SET_STATE', () => {
         const initialState = Map();
         const action = {
             type: 'SET_STATE',
-            state: Map({
-                deck: List.of(
-                    Map({rank: 'A', suit: 'C'}),
-                    Map({rank: '4', suit: 'S'}),
-                    Map({rank: 'T', suit: 'D'}),
-                    Map({rank: '2', suit: 'H'}),
-                    Map({rank: 'Q', suit: 'C'})
-                )
-            })
+            state: immutableState
         };
         const nextState = reducer(initialState, action);
-        expect(nextState).to.equal(fromJS({
-            deck: [
-                {rank: 'A', suit: 'C'},
-                {rank: '4', suit: 'S'},
-                {rank: 'T', suit: 'D'},
-                {rank: '2', suit: 'H'},
-                {rank: 'Q', suit: 'C'}
-            ]
-        }));
+        expect(nextState).to.equal(fromJS(jsState));
     });
 
     it('handles SET_STATE with plain JS payload', () => {
         const initialState = Map();
         const action = {
             type: 'SET_STATE',
-            state: {
-                deck: [
-                    {rank: 'A', suit: 'C'},
-                    {rank: '4', suit: 'S'},
-                    {rank: 'T', suit: 'D'},
-                    {rank: '2', suit: 'H'},
-                    {rank: 'Q', suit: 'C'}
-                ]
-            }
+            state: jsState
         };
         const nextState = reducer(initialState, action);
-        expect(nextState).to.equal(fromJS({
-            deck: [
-                {rank: 'A', suit: 'C'},
-                {rank: '4', suit: 'S'},
-                {rank: 'T', suit: 'D'},
-                {rank: '2', suit: 'H'},
-                {rank: 'Q', suit: 'C'}
-            ]
-        }));
+        expect(nextState).to.equal(fromJS(jsState));
     });
 
     it('handles SET_STATE without initial state', () => {
         const action = {
             type: 'SET_STATE',
-            state: {
-                deck: [
-                    {rank: 'A', suit: 'C'},
-                    {rank: '4', suit: 'S'},
-                    {rank: 'T', suit: 'D'},
-                    {rank: '2', suit: 'H'},
-                    {rank: 'Q', suit: 'C'}
-                ]
-            }
+            state: jsState
         };
         const nextState = reducer(undefined, action);
-
-        expect(nextState).to.equal(fromJS({
-            deck: [
-                {rank: 'A', suit: 'C'},
-                {rank: '4', suit: 'S'},
-                {rank: 'T', suit: 'D'},
-                {rank: '2', suit: 'H'},
-                {rank: 'Q', suit: 'C'}
-            ]
-        }));
+        expect(nextState).to.equal(fromJS(jsState));
     });
 
 });
