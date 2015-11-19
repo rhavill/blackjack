607578b5c971a16815b6b05ca4c5f8f6c0fba799
Ricardo Havill <rhavill2002@yahoo.com>
2015-11-18 19:59:25 -0500

    Added UI tests for Card component.

--- test/components/Card_spec.js
+++ test/components/Card_spec.js
@@ -8,9 +8,9 @@ import {renderIntoDocument, findRenderedDOMComponentWithTag}
 
 describe('Card', () => {
 
-    const aceOfDiamonds = cards[13];
-    const fourOfHearts = cards[29];
-    const jackOfClubs = cards[10];
+    let aceOfDiamonds = cards[13];
+    let fourOfHearts = cards[29];
+    let jackOfClubs = cards[10];
 
     it('renders a div with "face-down" background', () => {
         const component = renderIntoDocument(
@@ -19,6 +19,37 @@ describe('Card', () => {
         const div = findRenderedDOMComponentWithTag(component, 'div');
         expect(findDOMNode(div).style.getPropertyValue('background-image')).to.equal('url(cards.png)');
         expect(findDOMNode(div).style.getPropertyValue('background-position')).to.equal('0px 123px');
+    });
+
+    it('floats left', () => {
+        const component = renderIntoDocument(
+            <Card {...aceOfDiamonds} />
+        );
+        const div = findRenderedDOMComponentWithTag(component, 'div');
+        expect(findDOMNode(div).style.getPropertyValue('float')).to.equal('left');
+    });
+
+    it('renders correct background image position', () => {
+        aceOfDiamonds.isFaceUp = true;
+        const ace = renderIntoDocument(
+            <Card {...aceOfDiamonds} />
+        );
+        let div = findRenderedDOMComponentWithTag(ace, 'div');
+        expect(findDOMNode(div).style.getPropertyValue('background-position')).to.equal('0px -123px');
+
+        fourOfHearts.isFaceUp = true;
+        const four = renderIntoDocument(
+            <Card {...fourOfHearts} />
+        );
+        div = findRenderedDOMComponentWithTag(four, 'div');
+        expect(findDOMNode(div).style.getPropertyValue('background-position')).to.equal('-237px -246px');
+
+        jackOfClubs.isFaceUp = true;
+        const jack = renderIntoDocument(
+            <Card {...jackOfClubs} />
+        );
+        div = findRenderedDOMComponentWithTag(jack, 'div');
+        expect(findDOMNode(div).style.getPropertyValue('background-position')).to.equal('-790px 0px');
     });
 
 })
