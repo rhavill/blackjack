fbb88eeafd87f05e05e6b85fac93639e5f9ff6d1
Ricardo Havill <rhavill2002@yahoo.com>
2015-11-21 19:43:39 -0500

    Added Button component.

--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 
 import Card from './Card';
+import Button from './Button';
 
 class Blackjack extends Component {
 
@@ -10,8 +11,26 @@ class Blackjack extends Component {
         return this.props.cards;
     }
 
+    showButtons() {
+        switch (this.props.turn) {
+            case null:
+                return ['deal'];
+            case 'player':
+                return ['hit', 'stay'];
+            default:
+                return ['hit', 'deal', 'stay'];
+        }
+    }
+
     render() {
-        return <div style={{backgroundColor:'green'}}>
+        return <div>
+            <ReactCSSTransitionGroup transitionName="fade"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={300}>
+                {this.showButtons().map((text) => {
+                    return <Button key={text} text={text}/>
+                })}
+            </ReactCSSTransitionGroup>
             {this.getCards().map(card =>
                 <ReactCSSTransitionGroup   key={card.id}
                           transitionName="fadein"
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+export default class Button extends Component {
+    render() {
+        return <div className="button">
+            <div className="outer">
+                <div className="height">
+                    <div className="inner">{this.props.text}</div>
+                </div>
+            </div>
+        </div>;
+    }
+}
