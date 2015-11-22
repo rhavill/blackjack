e91521c6967d653e377f632d0f9f141f1388f5fb
Ricardo Havill <rhavill2002@yahoo.com>
2015-11-21 21:48:18 -0500

    Added handleClick function for Button component and its test.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export default class Button extends Component {
     render() {
-        return <div className="button">
+        return <div className="button" onClick={this.props.handleClick}>
             <div className="outer">
                 <div className="height">
                     <div className="inner">{this.props.text}</div>
--- test/components/Button_spec.js
+++ test/components/Button_spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {findDOMNode} from 'react-dom';
 import Button from '../../src/components/Button';
 import {expect} from 'chai';
-import {renderIntoDocument, scryRenderedDOMComponentsWithTag}
+import {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate}
     from 'react-addons-test-utils';
 
 describe('Button', () => {
@@ -20,4 +20,14 @@ describe('Button', () => {
         expect(divs[3].className).to.equal('inner');
     });
 
+    it('calls handleClick when clicked', () => {
+        let result;
+        const handleClick = () => result = 'clicked';
+        const component = renderIntoDocument(
+            <Button handleClick={handleClick} />
+        );
+        const divs = scryRenderedDOMComponentsWithTag(component, 'div')
+        Simulate.click(divs[0]);
+        expect(result).to.equal('clicked');
+    });
 })
