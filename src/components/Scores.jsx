c60a170a6ac53343a2af65d62c5291cf68566e5c
Ricardo Havill <rhavill2002@yahoo.com>
2015-12-01 18:42:50 -0500

    Position scores in center and fade-in when displaying scores.

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 import {List} from 'immutable'
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Scores extends Component {
 
     render() {
-
-        return <div><h3>scores</h3>
+        const style = {
+            display:'flex',
+            flexWrap:'wrap',
+            justifyContent: 'center'
+        };
+        return <div style={style}>
+            <ReactCSSTransitionGroup   key={1}
+                                       transitionName="fadein"
+                                       transitionAppear={true}
+                                       transitionAppearTimeout={500}
+                                       transitionEnter={false}
+                                       transitionLeave={false}>
+                <h3>scores</h3>
             {this.props.scores.map(score =>
 
                 {<p>score</p>}
             )}
+            </ReactCSSTransitionGroup>
         </div>
     }
 
